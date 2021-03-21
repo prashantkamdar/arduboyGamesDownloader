ba09976d14b9cd4c324415e568046f762ed4ef82
prashant <prashantkamdar@gmail.com>
2021-03-21 15:24:23 +0530

    hex folder, readme changes

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ for hexLink in hexLinks:
     link = hexLink['href']
     print(unquote(link.split('/')[-1]).replace('/','_'))
     r = requests.get(link)
-    with open(unquote(link.split('/')[-1]).replace('/','_'),'wb') as f:
+    #with open(unquote(link.split('/')[-1]).replace('/','_'),'wb') as f:
+    with open('hex\\' + unquote(link.split('/')[-1]).replace('/','_'),'wb') as f:
         f.write(r.content)
