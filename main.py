c248af67d184e15ee6a45653d1b875d724bb9c6c
prashant <prashantkamdar@gmail.com>
2021-03-21 14:54:11 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import unquote
+
+URL = 'https://arduboy.ried.cl/'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+hexLinks = soup.find_all('a', class_='style3')
+
+for hexLink in hexLinks:
+    link = hexLink['href']
+    print(unquote(link.split('/')[-1]).replace('/','_'))
+    r = requests.get(link)
+    with open(unquote(link.split('/')[-1]).replace('/','_'),'wb') as f:
+        f.write(r.content)
